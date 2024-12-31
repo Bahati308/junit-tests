6ff9328baff31dfe38ac0346c38e8124f9bd9b1b
Brian308 <bahatibrianp@gmail.com>
2024-12-31 03:55:49 +0300

    made some modifications

--- src/main/java/org/example/Basic.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Basic {
-    public static void main(String[] args) {
-
-        System.out.println("Hello, World!");
-    }
-}
---
+++ src/test/java/BasicTests.java
@@ -0,0 +1,15 @@
+import org.example.Basics;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BasicTests {
+
+    @Test
+    public void compare (){
+        Basics basicTests = new Basics();
+        int value = basicTests.compare(2,1);
+        Assertions.assertEquals(1,value);
+
+
+    }
+}
